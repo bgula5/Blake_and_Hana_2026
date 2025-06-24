551d308fcc8395a22f67726165ee29d0afec28ab
Blake <blakegula001@gmail.com>
2025-06-23 22:41:51 -0400

    updates

---
+++ web-ui/src/pages/Home.js
@@ -0,0 +1,18 @@
+import './images/backdrop.jpg';
+import './Home.css'
+
+
+function Home() {
+    return (
+            <div className="app-container">
+        <div className="arch-container">
+            <div className="centered-text">
+            <h1>Blake & Hana 2026</h1>
+            <p>A Journey of Love and Faith</p>
+            </div>
+        </div>
+        </div>
+    );
+    }
+
+export default Home;
